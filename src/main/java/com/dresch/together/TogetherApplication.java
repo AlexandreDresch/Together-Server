103b20ada8b197f3b98038bbeaf09c105d269d8c
AlexandreDresch <connectWithAlexandre@proton.me>
2024-07-17 23:26:15 -0300

    feat: add CORS

--- src/main/java/com/dresch/together/TogetherApplication.java
+++ src/main/java/com/dresch/together/TogetherApplication.java
@@ -2,8 +2,9 @@ package com.dresch.together;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class TogetherApplication {
 
 	public static void main(String[] args) {
